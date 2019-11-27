ae81db39912de9507dbc0cae44d95848ce32f769
Carolina <carolina.jimenez.g@gmail.com>
2019-11-27 15:23:21 -0300

    Adds START button and re-starts the game when pressing it

---
+++ button.py
@@ -0,0 +1,23 @@
+import pygame
+
+class Button:
+  def __init__(self, color, x, y, width, height, text=''):
+    self.color = color
+    self.x = x
+    self.y = y
+    self.width = width
+    self.height = height
+    self.text = text
+
+  def draw(self,surface):        
+    pygame.draw.rect(surface, self.color, (self.x , self.y, self.width, self.height), 0)
+    
+    if self.text != '':
+      font = pygame.font.SysFont('comicsans', 40)
+      text = font.render(self.text, 1, (0,0,0))
+      x = self.x + (self.width / 2 - text.get_width() / 2)
+      y = self.y + (self.height / 2 - text.get_height() / 2)
+      surface.blit(text, (x, y))
+
+  def was_clicked(self, x, y):
+    return x >= self.x and x <= (self.x + self.width) and y >= self.y and y <= (self.y + self.height)
--- main.py
+++ main.py
@@ -4,70 +4,98 @@ from grid import Grid
 from player import Player
 import players_handler
 import time
+from button import Button
  
-# ======= Initializations =========
-pygame.init()
+def run(elements):
+  done = False
+  screen = elements["screen"]
+  clock = elements["clock"]
+  grid = elements["grid"]
+  button = elements["button"]
+  player1 = elements["player1"]
+  player2 = elements["player2"]
+  curr_player = elements["curr_player"]
 
-size = (1000, 600) # [width, height]
-screen = pygame.display.set_mode(size)
- 
-pygame.display.set_caption("Tic-tac-toe")
+  while not done:
+    clicked_pos = -1
+    # --- Main event loop
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+          return False
+      if event.type == pygame.MOUSEBUTTONDOWN:
+        clicked_pos = pygame.mouse.get_pos()
 
-# Loop until the user clicks the close button.
-done = False
- 
-# Used to manage how fast the screen updates
-clock = pygame.time.Clock()
+    # --- Game logic should go here
+    if (clicked_pos != -1 and button.was_clicked(clicked_pos[0], clicked_pos[1])):
+      print("WAS CLICKED!")
+      return True
 
-grid = Grid()
-player1 = Player("X", True)
-player2 = Player("O", False)
+    if (curr_player.is_user() and clicked_pos != -1):
+      # grid.show_game_status() # just for debugging
+      moved = grid.set_position(clicked_pos[0], clicked_pos[1], curr_player.figure, True)
+      if (moved):
+        curr_player = players_handler.toggle_player(player1, player2)
+      
+      # grid.show_game_status() # just for debugging
 
-player1.set_turn(True)
-curr_player = player1
- 
-# -------- Main Program Loop -----------
-while not done:
-  clicked_pos = -1
-  # --- Main event loop
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-        done = True
-    if event.type == pygame.MOUSEBUTTONDOWN:
-      clicked_pos = pygame.mouse.get_pos()
+    if (not curr_player.is_user()):
+      [row, col] = curr_player.play(grid.get_game_status())
+      if (row != -1):
+        grid.set_position(col, row, curr_player.figure, False)
+        curr_player = players_handler.toggle_player(player1, player2)
 
-  # --- Game logic should go here
-  if (curr_player.is_user() and clicked_pos != -1):
-    # grid.show_game_status() # just for debugging
-    moved = grid.set_position(clicked_pos[0], clicked_pos[1], curr_player.figure, True)
-    if (moved):
-      curr_player = players_handler.toggle_player(player1, player2)
-    
-    # grid.show_game_status() # just for debugging
+    if (grid.is_end_game()):
+      done = True
+    # --- Screen-clearing code goes here
+    screen.fill(colors.WHITE)
 
-  if (not curr_player.is_user()):
-    [row, col] = curr_player.play(grid.get_game_status())
-    if (row != -1):
-      grid.set_position(col, row, curr_player.figure, False)
-      curr_player = players_handler.toggle_player(player1, player2)
+    # --- Drawing code should go here
+    grid.draw(screen)
+    button.draw(screen)
 
-  if (grid.is_end_game()):
-    done = True
-  # --- Screen-clearing code goes here
-  screen.fill(colors.WHITE)
+    # --- Go ahead and update the screen with what we've drawn.
+    pygame.display.flip()
+    pygame.display.update()
 
-  # --- Drawing code should go here
-  grid.draw(screen)
-
-  # --- Go ahead and update the screen with what we've drawn.
-  pygame.display.flip()
-  pygame.display.update()
-
-  # --- Limit to 60 frames per second
-  clock.tick(60)
+    # --- Limit to 60 frames per second
+    clock.tick(60)
   
-  if (done):
-    time.sleep(5) # to see what was the last move
- 
-# Close the window and quit.
-pygame.quit()
+  return False
+
+def initialize():
+  size = (1000, 600) # [width, height]
+  screen = pygame.display.set_mode(size)
+  
+  pygame.display.set_caption("Tic-tac-toe")
+  
+  # Used to manage how fast the screen updates
+  clock = pygame.time.Clock()
+
+  grid = Grid()
+  button = Button(colors.PURPLE, 750, 400, 200, 100, "START")
+  player1 = Player("X", True)
+  player2 = Player("O", False)
+
+  player1.set_turn(True)
+  curr_player = player1
+
+  elements = {}
+  elements["screen"] = screen
+  elements["clock"] = clock
+  elements["grid"] = grid
+  elements["button"] = button
+  elements["player1"] = player1
+  elements["player2"] = player2
+  elements["curr_player"] = curr_player
+  return elements
+
+if __name__ == "__main__":
+  pygame.init()
+  elements = initialize()
+  done = False
+  re_start = run(elements)
+  while (re_start and not done):
+    elements = initialize()
+    re_start = run(elements)
+    
+  pygame.quit()
